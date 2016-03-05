45713b75aa2b66376f706670aadaaada35d7c75d
HugoDF <hdifrancesco@gmail.com>
2016-03-05 11:23:19 +0000

    Slack button grabs client_id from .env

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,11 @@
 var hbs = require('hbs');
+require('dotenv').config();
 hbs.registerHelper("ifeq", function(a, b, options) {
     return (a === b)?options.fn(this): options.inverse(this);
 });
 
 hbs.registerHelper("slackbtn", function(){
-  return '<a href="https://slack.com/oauth/authorize?scope=commands&client_id=19358800983.24163094325"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>';
+  return '<a href="https://slack.com/oauth/authorize?scope=commands&client_id=' + process.env.SLACK_CLIENT_ID + '"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>';
 });
 
 hbs.registerHelper('test', function() {
